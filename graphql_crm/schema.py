3b2cb98b5bd7cbe55f87b0a83e376f97d419d96d
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-07-20 14:53:08 +0300

    update the structure

--- graphql_crm/schema.py
+++ graphql_crm/schema.py
@@ -3,11 +3,9 @@ from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 
 class Query(CRMQuery, graphene.ObjectType):
-    pass
+    hello = graphene.String()
 
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
